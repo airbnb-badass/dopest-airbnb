98394d1f16341a30e7fe72806848bb23ce5db161
matbios <mathieubarascou@gmail.com>
2014-08-19 15:01:53 +0200

    =list an apartment with the current  user id

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,5 +1,8 @@
 class ApartmentsController < ApplicationController
 
+  before_action :authenticate_user!, :only => :new
+
+
   def index
     @apartments = Apartment.all
   end
@@ -7,6 +10,23 @@ class ApartmentsController < ApplicationController
   def show
     @apartment = Apartment.find(params[:id])
     @owner = @apartment.user
+  end
+
+  def new
+    @apartment = Apartment.new
+  end
+
+  def create
+    @apartment = Apartment.new(apartment_params)
+    @apartment.user_id = current_user.id
+    @apartment.save
+    redirect_to apartments_path
+  end
+
+  private
+
+  def apartment_params
+    params.require(:apartment).permit(:title, :description, :price, :address, :zipcode, :city, :country)
   end
 
 end
