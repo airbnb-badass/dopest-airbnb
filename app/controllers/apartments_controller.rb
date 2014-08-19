88e912e9948f4a9600b672da1b20b60d75e4c66e
matbios <mathieubarascou@gmail.com>
2014-08-19 14:16:05 +0200

    listing + show

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -4,5 +4,9 @@ class ApartmentsController < ApplicationController
     @apartments = Apartment.all
   end
 
+  def show
+    @apartment = Apartment.find(params[:id])
+    @owner = @apartment.user
+  end
 
 end
